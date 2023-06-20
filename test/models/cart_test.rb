222df672b41486ed1cf3850b7b6ad18577d36350
Claire Greenwood <claire.greenwood@arenaflwoers.com>
2023-06-20 10:15:41 +0100

    cart model test added to test total_price method

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -6,7 +6,7 @@ class CartTest < ActiveSupport::TestCase
     @cart = Cart.new
   end
 
-  test "add_product should increase the quantity of an exisiting line item by one" do
+  test "add_product increases the quantity of an exisiting line item by one" do
     line_item = @cart.add_product(products(:one))
     assert line_item.save
     assert_equal 1, @cart.line_items.size
@@ -14,15 +14,30 @@ class CartTest < ActiveSupport::TestCase
     line_item = @cart.add_product(products(:one))
     assert line_item.save
     assert_equal 1, @cart.line_items.size
-    assert_equal 2, @cart.line_items[0].quantity
+    assert_equal 2, line_item.quantity
   end
 
-  test "add_product should create a new line item for non-exisiting products" do
+  test "add_product creates a new line item for non-exisiting products" do
     line_item_one = @cart.add_product(products(:one))
     line_item_two = @cart.add_product(products(:two))
 
     assert line_item_one.save
     assert line_item_two.save
     assert_equal 2, @cart.line_items.size
+  end
+
+  test "total_price calculates the correct total price of the cart" do
+    product1 = (products(:one))
+    product2 = (products(:ruby))
+    line_item_one = LineItem.new(product: product1, quantity: 2)
+    line_item_two = LineItem.new(product: product2, quantity: 3)
+    @cart.line_items << line_item_one
+    @cart.line_items << line_item_two
+
+    expected_total_price = (product1.price * line_item_one.quantity) + (product2.price * line_item_two.quantity)
+
+    total_price = @cart.total_price
+
+    assert_equal expected_total_price, total_price
   end
 end
