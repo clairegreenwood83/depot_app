cc351874af4500622333fb29a1ef81bb3647b5be
Claire Greenwood <claire.greenwood@arenaflwoers.com>
2023-06-22 17:29:48 +0100

    links to administration functions added to the sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authorize #authorize() method is invoked before every action in application
+
+    protected
+
+      def authorize
+        unless User.find_by(id: session[:user_id])
+            redirect_to login_url, notice: "Please log in"
+        end
+      end
 end
