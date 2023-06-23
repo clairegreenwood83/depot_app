8bad832c18b2982031ebc22817295ef1a57299fe
Claire Greenwood <claire.greenwood@arenaflwoers.com>
2023-06-23 12:59:55 +0100

    system test added to assert that the entire checkout process, communication with the payment processor and sending email is successful

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "depot_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "",
+    password: "",
+    enable_starttls_auto: true
+  }
+end
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
