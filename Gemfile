d2d533be3ae8b377f3186e6b37993817082bda7e
Claire Greenwood <claire.greenwood@arenaflwoers.com>
2023-06-29 12:03:45 +0100

    Action Mailbox setup

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "3.1.3"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.5"
 
+gem 'actiontext'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -49,7 +51,7 @@ gem "bootsnap", require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'support_requests/index'
   get 'admin' => 'admin#index'
   controller :sessions do
     get 'login' => :new
@@ -8,9 +9,9 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
 
-  # START_HIGHLIGHT
+  
   resources :support_requests, only: %i[index update]
-  # END_HIGHLIGHT
+ 
   resources :users
   resources :orders
   resources :line_items do
@@ -21,7 +22,7 @@ Rails.application.routes.draw do
   resource :cart, only: [:show, :create, :update, :destroy]
   root 'store#index', as: 'store_index', via: :all
   resources :products do 
-    get :who_bought, on: :memeber
+    get :who_bought, on: :member
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
